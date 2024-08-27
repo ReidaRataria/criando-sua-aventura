ee2a6cfe9a5a49a6e535956bec03fdb5e9a5b2f1
ReidaRataria <lgcp.2006@gmail.com>
2024-08-26 21:45:44 -0300

    Projeto final

---
+++ script.js
@@ -0,0 +1,10 @@
+const avanca = document.querySelectorAll('.btn-proximo');
+avanca.forEach(button =>{
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximopasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementByld(proximopasso).classList.add('ativo');
+    })
+})
